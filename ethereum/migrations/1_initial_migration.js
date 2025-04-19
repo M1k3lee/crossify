41e4f20e01d45b0ef465f01437af041a5c996817
mikeleigh30 <65546567+mikeleigh30@users.noreply.github.com>
2025-04-19 01:26:29 +0100

    Create 1_initial_migration.js

---
+++ ethereum/migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const TokenFactory = artifacts.require("TokenFactory");
+
+module.exports = async function(deployer) {
+  // Initial migration - just to initialize the deployment
+  console.log("Initial migration completed");
+};
